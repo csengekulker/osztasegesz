cd3eb602ab1821d55934be8f9afe99a754b2684b
csengekulker <k71608883429@ktch.hu>
2022-02-16 17:34:50 +0100

    wrote inputs and input variables

--- Osztasegesz.java
+++ Osztasegesz.java
@@ -14,5 +14,15 @@ class Osztasegesz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Balogh Csenge, Szoft_I_N, 2022.02.16");
+        System.out.println("Osztas hanyadosa, maradeka");
+
+        System.out.print("Szamlalo (osztando) > ");
+        int numer = sc.nextInt();
+        System.out.print("Nevezo (oszto) > ");
+        int denom = sc.nextInt();
+
+        sc.close();
+
     }
 }
