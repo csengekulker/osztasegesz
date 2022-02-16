48c6f4692cd3ff4e89efbae5fc75bb3db59f7a53
csengekulker <k71608883429@ktch.hu>
2022-02-16 17:32:13 +0100

    main class template ready

---
+++ Osztasegesz.java
@@ -0,0 +1,18 @@
+/*
+* File: Osztasegesz.java
+* Author: Balogh Csenge
+* Copyright: 2022, Balogh Csenge
+* Group: Szoft_I_N
+* Date: 2022-02-16
+* Github: https://github.com/csengekulker/
+* Licenc: GNU GPL
+*/
+
+import java.util.Scanner;
+
+class Osztasegesz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
